a7cc3ebe2c6915cc4dde5881f1c4254c31f22057
seobin0224 <binsong2@sookmyung.ac.kr>
2024-04-28 20:19:25 +0900

    [3주차 웹 기초 스터디] 윤서빈

---
+++ 윤서빈/3주차_실습/name.js
@@ -0,0 +1,5 @@
+const myname = "윤서빈";
+const introduction = "제 이름은 " + myname + "입니다.";
+
+console.log(introduction);
+
---
+++ 윤서빈/3주차_실습/power.js
@@ -0,0 +1,47 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const pokemons = document.querySelectorAll(".container .pokemons .pokemon");
+  const compareButton = document.getElementById("calculate");
+
+  const pokemonStats = {};
+
+  function generateRandomStats() {
+    return Math.floor(Math.random() * 500) + 1;
+  }
+
+  pokemons.forEach((pokemon, index) => {
+    const stat = generateRandomStats();
+    const pokemonNumber = index;
+    pokemonStats[pokemonNumber] = stat;
+    pokemon.addEventListener("click", function () {
+      alert(`monster${pokemonNumber}의 능력치는 ${stat}입니다.`);
+    });
+  });
+
+  compareButton.addEventListener("click", function () {
+    const monster1 = parseInt(prompt("monster1을 선택해주세요. (숫자 입력)"));
+    const monster2 = parseInt(prompt("monster2을 선택해주세요.(숫자입력)"));
+
+    if (
+      isNaN(monster1) ||
+      isNaN(monster2) ||
+      monster1 < 0 ||
+      monster1 >= pokemons.length ||
+      monster2 < 0 ||
+      monster2 >= pokemons.length
+    ) {
+      alert(`값이 올바르지 않습니다.`);
+      return;
+    }
+
+    const monster1Stat = pokemonStats[monster1];
+    const monster2Stat = pokemonStats[monster2];
+
+    if (monster1Stat > monster2Stat) {
+      alert(`몬스터 ${monster1} 이 이깁니다`);
+    } else if (monster1Stat < monster2Stat) {
+      alert(`몬스터 ${monster2} 이 이깁니다.`);
+    } else {
+      alert("능력치가 동일합니다");
+    }
+  });
+});
