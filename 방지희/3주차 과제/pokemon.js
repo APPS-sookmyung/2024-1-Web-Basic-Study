0c4a2b1cc0bf48339cc6d19f84a3e375f8eeca14
Bangjh05 <everangel05@naver.com>
2024-05-01 23:53:09 +0900

    Fix: 3주차 과제 수정

--- 방지희/3주차 과제/pokemon.js
+++ 방지희/3주차 과제/pokemon.js
@@ -11,7 +11,7 @@ pokemons.forEach((pokemon, index) => {
     const pokemonNumber = index;
     pokemonStats[pokemonNumber] = stat;
     pokemon.addEventListener("click", function () {
-        alert('monster'+pokemonNumber+'의 능력치는 '+stat+'입니다.');
+        alert(`monster ${pokemonNumber}의 능력치는 ${stat}입니다.`);
     });
 });
 
@@ -19,15 +19,8 @@ calculateButton.addEventListener("click", function () {
     const monster1 = parseInt(prompt("monster1을 선택해주세요. (숫자 입력)"));
     const monster2 = parseInt(prompt("monster2를 선택해주세요. (숫자 입력)"));
 
-    if (
-        isNaN(monster1) ||
-        isNaN(monster2) ||
-        monster1 < 0 ||
-        monster1 >= pokemons.length ||
-        monster2 < 0 ||
-        monster2 >= pokemons.length
-    ) {
-        alert('값이 올바르지 않습니다.');
+    if (isNaN(monster1) || isNaN(monster2)) {
+        alert("값이 올바르지 않습니다.");
         return;
     }
 
@@ -35,9 +28,9 @@ calculateButton.addEventListener("click", function () {
     const monster2Stat = pokemonStats[monster2];
 
     if (monster1Stat > monster2Stat) {
-        alert('몬스터 '+monster1+' 이 이깁니다.');
+        alert(`몬스터 ${monster1} 이 이깁니다.`);
     } else if (monster1Stat < monster2Stat) {
-        alert('몬스터 '+monster2+' 이 이깁니다.');
+        alert(`몬스터 ${monster2} 이 이깁니다.`);
     } else {
         alert("능력치가 동일합니다.");
     }
