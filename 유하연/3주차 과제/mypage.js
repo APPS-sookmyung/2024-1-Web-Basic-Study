b4fd953a1d01da9c4929834aac2aa61d3468d821
Heoooooo <hayeonyu.1@gmail.com>
2024-05-02 15:05:39 +0900

    3주차 수정1

--- 유하연/3주차 과제/mypage.js
+++ 유하연/3주차 과제/mypage.js
@@ -15,14 +15,6 @@ pokemonImages.forEach((image, index) => {
   });
 });
 
-// "능력치 계산" 버튼을 동적으로 생성
-const calculateButton = document.createElement('button');
-calculateButton.textContent = '능력치 계산';
-calculateButton.id = 'calculateBtn';
-
-// "container" 요소에 "능력치 계산" 버튼 추가
-document.querySelector('.container').appendChild(calculateButton);
-
 // 포켓몬 목록
 const pokemons = Array.from({ length: 32 }, (_, index) => (index + 1).toString());
 
@@ -32,16 +24,14 @@ function selectPokemon() {
   const pokemon2Index = parseInt(prompt('monster2를 선택해주세요(숫자입력)'));
 
   // 유효한 인덱스인지 확인
-  if (isNaN(pokemon1Index) || isNaN(pokemon2Index) || pokemon1Index < 1 || pokemon1Index > pokemons.length || pokemon2Index < 1 || pokemon2Index > pokemons.length) {
-    alert('값이 올바르지않습니다.');
-    return;
-  }
+  if (isNaN(pokemon1Index) || isNaN(pokemon2Index) || pokemon1Index < 1 || pokemon1Index > pokemons.length || pokemon2Index < 1 || pokemon2Index > pokemons.length) return alert('값이 올바르지 않습니다.');
+
 
   return [pokemon1Index - 1, pokemon2Index - 1]; // 배열 인덱스는 0부터 시작하므로 입력값에서 1을 뺍니다.
 }
 
 // "능력치 계산" 버튼에 이벤트 추가
-calculateButton.addEventListener('click', () => {
+document.getElementById('calculateBtn').addEventListener('click', () => {
   const selectedPokemonIndexes = selectPokemon();
   if (!selectedPokemonIndexes) return;
 
