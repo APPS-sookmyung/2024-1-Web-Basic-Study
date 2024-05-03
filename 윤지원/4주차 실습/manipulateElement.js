d5e4a2d2bf714f23dc63a95df06dae9edaf7361e
rosaze <geenieeyoon@gmail.com>
2024-05-03 23:15:03 +0900

    Revert "Fix: 에러 파일 제거"
    This reverts commit ce971ba737c3d0130c0b8a373ef7aad83ea229ae.

--- 윤지원/4주차 실습/manipulateElement.js
+++
@@ -1,9 +0,0 @@
-/*const button = document.querySelector("#btn");
-console.log(button.classList);
-
-if (button.classList.contains("add")) {
-  button.classList.remove("add");
-} else {
-  button.classList.add("add");
-}
-console.log(button.classList);*/
