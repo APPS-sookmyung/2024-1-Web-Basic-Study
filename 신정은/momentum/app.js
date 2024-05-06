cd976753bb62cf812be952c29dfade3770d10aff
eungrond <jeshin@sookmyung.ac.kr>
2024-05-06 12:57:32 +0900

    Feat: #2.6 실습 코드

--- 신정은/momentum/app.js
+++ 신정은/momentum/app.js
@@ -1,9 +1,9 @@
-const daysOfWeek = ["mon", "tue", "wed", "thu", "fri", "sat"];
-
-// Get Item from Array
-console.log(daysOfWeek);
-
-// Add one more day to the array
-daysOfWeek.push("sun");
-
-console.log(daysOfWeek);
+const player = {
+    name: "jungeun",
+    points: 10,
+    fat: true,
+};
+console.log(player);
+player.lastName="potato";
+player.points= player.points + 15;
+console.log(player);
