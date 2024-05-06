85bf0fdcdab11869fc78ba5cc08d5c5d9c5c1e28
suminb99 <suminbaek99@gmail.com>
2024-05-07 00:03:40 +0900

    Feat: #2.7 실습 코드

--- 백수민/Momentum/app.js
+++ 백수민/Momentum/app.js
@@ -1,19 +1,7 @@
-const player = {
-    name: "nico",
-    points: 10,
-    fat: true,   
-};
+function sayHello() {
+    console.log("Hello!");
+}
 
-console.log(player);
-console.log(player.name); 
-
-console.log(player.name);
-console.log(player["name"]);
-
-console.log(player.fat); // true
-player.fat = false;
-console.log(player.fat); // false
-
-player.lastName = "potato";
-player.points = player.points + 15;
-console.log(player.points);
+sayHello(); // "Hello!"
+sayHello(); // "Hello!"
+sayHello(); // "Hello!"
