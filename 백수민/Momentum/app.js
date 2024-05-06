2dc9c2f70624c7e1a047a6e34ba75f8067772a65
suminb99 <suminbaek99@gmail.com>
2024-05-06 23:54:06 +0900

    Feat: #2.2 실습 코드

--- 백수민/Momentum/app.js
+++ 백수민/Momentum/app.js
@@ -1,1 +1,12 @@
-alert("Hi");
+const a = 5;
+const b = 2;
+const myName = "John"
+
+console.log(5 + 2);
+console.log(5 * 2);
+console.log(5 / 2);
+
+console.log(a + b); 
+console.log(a * b); 
+console.log(a / b);
+console.log("hello " + myName);
