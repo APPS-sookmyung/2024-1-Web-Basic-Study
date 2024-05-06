2c0542dca6ada6aa491e1646770b95aab44e567b
suminb99 <suminbaek99@gmail.com>
2024-05-07 00:05:00 +0900

    Feat: #2.8 실습 코드

--- 백수민/Momentum/app.js
+++ 백수민/Momentum/app.js
@@ -1,7 +1,29 @@
-function sayHello() {
-    console.log("Hello!");
+function sayHello(nameOfPerson, age) {
+    console.log("Hello my name is " + nameOfPerson + " and I'm " + age);
 }
 
-sayHello(); // "Hello!"
-sayHello(); // "Hello!"
-sayHello(); // "Hello!"
+sayHello("nico", 10); // "Hello my name is nico and I'm 10
+sayHello("dal", 23); // "Hello my name is dal and I'm 23
+sayHello("lynn", 21); // "Hello my name is lynn and I'm 21
+
+function plus(firstNum, secondNum) {
+    console.log(firstNum + secondNum);
+}
+
+function divide(a, b) {
+    console.log(a / b);
+}
+
+console.log(firstNum);
+plus(8, 60); // 68
+divide(98, 20); // 4.9
+
+const player = {
+    name: "nico",
+    sayHello: function (otherPersonsName) {
+        console.log("hello " +  otherPersonsName + " nice to meet you!");
+    }
+}
+
+console.log(player.name); // nico
+player.sayHello("lynn"); // hello lynn nice to meet you!
