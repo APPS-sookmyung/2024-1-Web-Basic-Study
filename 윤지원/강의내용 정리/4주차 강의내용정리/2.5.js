6dd62bb29b8f3377b29d403520139967a041390c
윤지원 <133832857+rosaze@users.noreply.github.com>
2024-05-07 04:56:47 +0900

    Feat: 2.5 실습 코드

--- 윤지원/강의내용 정리/4주차 강의내용정리/2.5.js
+++ 윤지원/강의내용 정리/4주차 강의내용정리/2.5.js
@@ -4,6 +4,6 @@ const player = {
   fat: true,
 };
 console.log(player.name);
-console.log(player["name"]); //다른 방법
+console.log(player["name"]);
 player.points = player.points + 15;
 console.log(player.points);
