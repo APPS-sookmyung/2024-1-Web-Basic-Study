cc2c553a73db0e57c07f5cd02f07e17ef22e4d43
suminb99 <suminbaek99@gmail.com>
2024-05-06 23:59:53 +0900

    Feat: #2.5 실습 코드

--- 백수민/Momentum/app.js
+++ 백수민/Momentum/app.js
@@ -1,5 +1,19 @@
-const amIFat = null;
-let something;
+const mon = "mon";
+const tue = "tue";
+const wed = "wed";
+const thu = "thu";
+const fri = "fri";
+const sat = "sat";
+const sun = "sun";
 
-console.log(amIFat); 
-console.log(something);
+const daysOfWeek = [mon, tue, wed, thu, fri, sat]; // array
+
+// Get Item from Array
+console.log(daysOfWeek[5]); // sat
+
+// Add one more day to the array
+daysOfWeek.push("sun");
+console.log(daysOfWeek);
+
+const toBuy = ["potato", "tomato", "pizza"];
+toBuy.push("kimbap");
