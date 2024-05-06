a3b02f48126b73a0882513ccd098be7f2febdc15
yoon-jr <yojr3001@sookmyung.ac.kr>
2024-05-06 17:17:42 +0900

    Docs: 4주차 강의 내용 정리

---
+++ 윤정란/4주차 실습/momentum/app.js
@@ -0,0 +1,9 @@
+const player = {
+  name: "jr",
+  sayHello: function (orderPersonName) {
+    console.log("hello " + orderPersonName + " nice to meet you");
+  },
+};
+
+player.sayHello("lynn");
+player.sayHello("nico");
