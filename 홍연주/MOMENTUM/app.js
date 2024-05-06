cb6985ed702cefb07fdd4b87b2266f0a5a2c9bf8
y-Joo492 <phyj492@naver.com>
2024-05-06 18:16:07 +0900

    Feat: #2.3 실습 코드(1)

--- 홍연주/MOMENTUM/app.js
+++ 홍연주/MOMENTUM/app.js
@@ -1,8 +1,12 @@
 const a = 5;
 const b = 2;
-const myName = "Hyj";
+let myName = "Hyj";
 
 console.log(a + b);
 console.log(a * b);
 console.log(a / b);
 console.log("hello" + myName);
+
+myName = "hongyeonju";
+
+console.log("my new name is " + myName);
