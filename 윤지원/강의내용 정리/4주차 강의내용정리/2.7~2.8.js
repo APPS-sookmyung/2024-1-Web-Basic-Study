4db81d28b78b01e3f575ea399c7730211fbb0ac9
윤지원 <133832857+rosaze@users.noreply.github.com>
2024-05-07 04:58:26 +0900

    Feat: 2.7~2.8 실습 코드

--- 윤지원/강의내용 정리/4주차 강의내용정리/2.7~2.8.js
+++ 윤지원/강의내용 정리/4주차 강의내용정리/2.7~2.8.js
@@ -3,7 +3,7 @@ const player = {
   sayHello: function (otherpersonsname) {
     console.log("hello! " + otherpersonsname + " nice to meet you");
   },
-}; // 함수 안에 함수 정의
+};
 console.log(player.name);
 player.sayHello("lynn");
 player.sayHello("지원이");
