88f70574f87db01c1045c6a19de5cac8999016d0
eungrond <jeshin@sookmyung.ac.kr>
2024-05-06 11:53:57 +0900

    Feat: #2.3 실습 코드

--- 신정은/momentum/app.js
+++ 신정은/momentum/app.js
@@ -1,9 +1,12 @@
 const a=5;
 const b=2;
-
 const myName="jungeun";
 
 console.log(a+b);
 console.log(a*b);
 console.log(a/b);
 console.log("hello "+myName);
+
+myName = "shinjungeun";
+
+console.log("your new name is "+myName);
