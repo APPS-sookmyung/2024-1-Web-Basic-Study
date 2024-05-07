8e1f246678c3bf78fb9ee3bf38423c4a392ac031
j1m1n-ha <163178666+j1m1n-ha@users.noreply.github.com>
2024-05-07 18:41:56 +0900

    Feat: #2.5 실습 코드

--- 하지민/momentum/app.js
+++ 하지민/momentum/app.js
@@ -1,3 +1,13 @@
-const amIfat = null;
-let something;
-console.log(something, amIfat);
+const daysOfWeek = ["mon", "tue", "wed", "thu", "fri", "sat"];
+
+console.log(daysOfWeek);
+// Get Item from Array
+
+// Add ine more day to the array
+daysOfWeek.push("sun");
+
+console.log(daysOfWeek);
+
+const toBuy = ["potato", "tomato", "pizza"];
+toBuy.push("kimbab");
+console.log(toBuy[9999]);
