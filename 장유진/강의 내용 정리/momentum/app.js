ba5369bc94a78934c94bbd2b6a01b75955dc25e4
장유진 (uzin) <139611151+janguzin@users.noreply.github.com>
2024-05-07 16:17:50 +0900

    Feat: #2.5 실습 코드

--- 장유진/강의 내용 정리/momentum/app.js
+++ 장유진/강의 내용 정리/momentum/app.js
@@ -1,3 +1,12 @@
-const amIFat = null;
-let something;
-console.log(something, amIFat)
+const daysOfWeek = ["mon", "tue", "wed", "thu", "fri", "sat"];
+
+console.log(daysOfWeek[5]);
+
+console.log(daysOfWeek);
+
+daysOfWeek.push("sun");
+
+console.log(daysOfWeek);
+
+const toBuy = ["potato", "tomato", "pizza"];
+console.log(toBuy[2]);
