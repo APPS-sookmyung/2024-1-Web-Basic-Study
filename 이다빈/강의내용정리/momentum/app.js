ae5707d095d99373b4fba6ef7555f850e6a25bbf
ninum12 <ninum@sookmyung.ac.kr>
2024-05-07 17:37:29 +0900

    Feat: #2.5 실습 코드

--- 이다빈/강의내용정리/momentum/app.js
+++ 이다빈/강의내용정리/momentum/app.js
@@ -1,6 +1,15 @@
-// const amIFat = true;
-// console.log(amIFat);
+const daysOfWeek = ["mon", "tue", "wed", "thu", "fri", "sat"];
+// 계속해서 variable을 만들 수 없음
+const nosense = [1, 2, "hello", false, null, true, undefined, "nico"];
+// 숫자 문자 boolean 등 다 가능
+console.log(nosense);
 
-const amIFat = null;
-let something;
-console.log(something); //undefined가 출력
+console.log(daysOfWeek);
+// Get Item from Array
+console.log(daysOfWeek[4]); //0부터 시작하므로 다섯번째 항목을 받기 위해선 4
+
+//Add one more day to the array
+daysOfWeek.push("sun");
+
+const toBuy = ["potato", "tomato", "pizza"];
+toBuy.push("kimbab");
