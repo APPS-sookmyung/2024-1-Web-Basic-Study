902e34318168f7e9d3b902e3d8d5e49ba67e3194
seobin0224 <binsong2@sookmyung.ac.kr>
2024-05-07 19:28:02 +0900

    Feat: 2.8 실습코드

--- 윤서빈/momentum/app.js
+++ 윤서빈/momentum/app.js
@@ -1,8 +1,11 @@
-function sayHello() {
-  console.log("Hello!");
+function plus(firstNumber, secondNumber) {
+  console.log(firstNumber + secondNumber);
 }
+function divide(a, b) {
+  console.log(a / b);
+}
+plus(8, 60);
+divide(98, 20);
 
-sayHello();
-sayHello();
-sayHello();
-sayHello();
+console.log();
+player.sayhello();
