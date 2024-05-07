d71d7b70f547c64914b7e15d6341e30525df032b
seobin0224 <binsong2@sookmyung.ac.kr>
2024-05-07 13:34:44 +0900

    Feat: #2.0 실습 코드

---
+++ 윤서빈/4주차_실습/selectElement.js
@@ -0,0 +1,11 @@
+//const findGroup = document.getElementsByClassName("find");
+//const [btn, span] = findGroup;
+
+//btn.innerHTML = "찾았다!";
+//span.innerHTML = "찾았다!";
+
+//const queryBtn1 = document.querySelector("div .find:first-child");
+//const queryBtn2 = document.querySelector("#btn");
+
+//queryBtn1.style.backgroundColor = "green";
+//queryBtn2.style.color = "red";
