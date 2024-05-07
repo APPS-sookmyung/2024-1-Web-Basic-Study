26caa4f0c2432c5e42d83cb956289e5a6f473556
Heoooooo <hayeonyu.1@gmail.com>
2024-05-07 23:19:29 +0900

    #2.6 실습 코드

--- 유하연/momentum/app.js
+++ 유하연/momentum/app.js
@@ -1,17 +1,10 @@
-const mon = "mon";
-const tue = "tue";
-const wed = "wed";
-const thu = "thu";
-const fri = "fri";
-const sat = "sat";
-const sun = "sund";
+const player ={
+    name : "nico",
+    points : 10,
+    fat : true,
+}
 
-const daysOfWeek = ["mon", "tue", "wed", "thu", "fri", "sat"];
-
-//get Item from Array
-console.log(daysOfWeek);
-
-//Add one more day to the array
-daysOfWeek.push("sun")
-console.log(daysOfWeek);
-
+console.log(player);
+player.lastName = "potato";
+player.points = player.points + 15;
+console.log(player);
