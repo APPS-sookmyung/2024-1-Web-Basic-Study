f921ac80bca954bc2aa3087d67f6bdc3d5e4e869
ninum12 <ninum@sookmyung.ac.kr>
2024-05-07 17:35:49 +0900

    Feat: #2.4 실습 코드

--- 이다빈/강의내용정리/momentum/app.js
+++ 이다빈/강의내용정리/momentum/app.js
@@ -1,14 +1,6 @@
-console.log(54545454); //숫자나 문자를 넣을 수 있음. console에 출력
+// const amIFat = true;
+// console.log(amIFat);
 
-console.log(5 + 2); //위에서 아래로
-console.log(5 * 2);
-console.log(5 / 2);
-
-const a = 5;
-const b = 2;
-const myName = "nico"
-
-console.log(a + b);
-console.log(a * b);
-console.log(a / b);
-console.log("hello " + myName);
+const amIFat = null;
+let something;
+console.log(something); //undefined가 출력
