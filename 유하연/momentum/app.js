36bea6c517ece7d186fe1903ac24bce33695d35e
Heoooooo <hayeonyu.1@gmail.com>
2024-05-07 22:58:36 +0900

    #2.5 실습 코드

---
+++ 유하연/momentum/app.js
@@ -0,0 +1,17 @@
+const mon = "mon";
+const tue = "tue";
+const wed = "wed";
+const thu = "thu";
+const fri = "fri";
+const sat = "sat";
+const sun = "sund";
+
+const daysOfWeek = ["mon", "tue", "wed", "thu", "fri", "sat"];
+
+//get Item from Array
+console.log(daysOfWeek);
+
+//Add one more day to the array
+daysOfWeek.push("sun")
+console.log(daysOfWeek);
+
