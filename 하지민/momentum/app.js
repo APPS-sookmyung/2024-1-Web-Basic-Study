3bd88543960f1497a983d451e988fe9b53c386bd
j1m1n-ha <163178666+j1m1n-ha@users.noreply.github.com>
2024-05-07 14:44:24 +0900

    Feat: #2.3 실습 코드

--- 하지민/momentum/app.js
+++ 하지민/momentum/app.js
@@ -1,10 +1,11 @@
-console.log(545454);
-console.log("lalalala");
 const a = 5;
 const b = 2;
-const myName = "nico";
-const veryLongVariableName = 0;
+let myName = "nico";
+
 console.log(a + b);
 console.log(a * b);
 console.log(a / b);
 console.log("hello " + myName);
+
+myName = "nicolas";
+console.log("your new name is " + myName);
