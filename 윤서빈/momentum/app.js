09aa76d926e5e88e1ee0b083c39af1c6578faf90
seobin0224 <binsong2@sookmyung.ac.kr>
2024-05-07 15:43:39 +0900

    Feat: 2.4 실습코드

--- 윤서빈/momentum/app.js
+++ 윤서빈/momentum/app.js
@@ -1,12 +1,3 @@
-const a = 5;
-const b = 2;
-let myName = "bini";
-
-console.log(a + b);
-console.log(a * b);
-console.log(a / b);
-console.log("hello " + myName);
-
-myName = "seobin";
-
-console.log("your new name is " + myName);
+const amIFat = null;
+let something;
+console.log(something);
