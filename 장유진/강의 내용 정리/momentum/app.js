9c6a89837c82f10f1f31e3bac213e417ba55f727
장유진 (uzin) <139611151+janguzin@users.noreply.github.com>
2024-05-07 16:03:39 +0900

    Feat: #2.4 실습 코드

--- 장유진/강의 내용 정리/momentum/app.js
+++ 장유진/강의 내용 정리/momentum/app.js
@@ -1,11 +1,3 @@
-const a = 5;
-const b = 5;
-let myName = "유진";
-
-console.log(a+b);
-console.log(a*b);
-console.log(a/b);
-console.log("hello " + myName);
-
-myName = "장유진";
-console.log("my name is " + myName);
+const amIFat = null;
+let something;
+console.log(something, amIFat)
