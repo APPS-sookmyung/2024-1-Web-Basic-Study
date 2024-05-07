ed9bc4cdd0e32d92a4fa641d5ae92232e097e1a9
seobin0224 <binsong2@sookmyung.ac.kr>
2024-05-07 16:28:15 +0900

    Feat: 2.6 실습코드

--- 윤서빈/momentum/app.js
+++ 윤서빈/momentum/app.js
@@ -1,20 +1,11 @@
-const mon = "mon";
-const tue = "tue";
-const wed = "wed";
-const thu = "thu";
-const fri = "fri";
-const sat = "sat";
-const sun = "sun";
+const player = {
+  name: "bini",
+  points: 10,
+  fat: true,
+};
+console.log(player);
+console.log(player.name);
+console.log(player["name"]);
 
-const daysOfWeek = [mon, tue, wed, thu, fri, sat];
-
-const nonsense = [1, 2, "hello", false, null, true, undefined, "bini"];
-
-//get item from array
-console.log(daysOfWeek[4]);
-console.log(daysOfWeek);
-
-//add one more day to the array
-daysOfWeek.push("sun");
-
-console.log(daysOfWeek);
+player.fat = false;
+console.log(player);
