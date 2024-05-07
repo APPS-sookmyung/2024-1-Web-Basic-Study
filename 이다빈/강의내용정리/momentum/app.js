920099cf72acbd59d8ec200e53fb332659236cd7
ninum12 <ninum@sookmyung.ac.kr>
2024-05-07 17:40:23 +0900

    Feat: #2.7 실습 코드

--- 이다빈/강의내용정리/momentum/app.js
+++ 이다빈/강의내용정리/momentum/app.js
@@ -1,20 +1,7 @@
-const playerName = "nico";
-const playerPoints = 121212;
-const plyaerHansome = false;
-const playerFat = "little bit";
-
-// const player = ["nico", 121212, false, "little bit"];
-
-//데이터를 최선으로 정리하는 방식 -> object
-const player = {
-  name: "nico", // = 대신 : 사용
-  points: 10,
-  fat: true,
-};
-console.log(player);
-console.log(player.name);
-console.log(player["name"]); //위 코드와 같음
-
-player.fat = false; //수정
-player.lastName = "potato"; //추가
-player.points = player.points + 15;
+//바뀌는 부분은 이름뿐
+function sayHello() {
+  console.log("Hello my name is C");
+}
+sayHello("nico");
+sayHello("dal");
+sayHello("lynn");
