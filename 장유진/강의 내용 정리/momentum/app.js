78c43b2837aff7503655485d15b91385328400fc
장유진 (uzin) <139611151+janguzin@users.noreply.github.com>
2024-05-07 16:30:47 +0900

    Feat: #2.7 실습 코드

--- 장유진/강의 내용 정리/momentum/app.js
+++ 장유진/강의 내용 정리/momentum/app.js
@@ -1,17 +1,9 @@
-const player = {
-    name: "nico",
-    points: 10,
-    fat: true,
-};
-console.log(player);
-console.log(player.name);
-console.log(player["name"]);
+function sayHello() {
+    console.log("hello my name is C")
+}
 
-player.fat = false; // 변경
-console.log(player);
-
-player.lastName = "potato"; // 추가
-console.log(player);
-
-player.points += 15;
-console.log(player.points);
+alert();
+sayHello();
+sayHello();
+sayHello();
+sayHello("nico");
