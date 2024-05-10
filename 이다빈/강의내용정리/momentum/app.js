ee760b50aee68738d03dee6c3cbb0cd2b67c57a7
ninum12 <ninum@sookmyung.ac.kr>
2024-05-10 21:22:19 +0900

    Feat: #2.10 실습 코드

--- 이다빈/강의내용정리/momentum/app.js
+++ 이다빈/강의내용정리/momentum/app.js
@@ -1,27 +1,20 @@
-function sayHello(nameOfPerson, age) {
-  console.log("Hello my name is" + nameOfPerson + " and I'm " + age);
-}
-sayHello("nico", 10); //함수 sayHello에게 "nico" 와 10을 보냄.
-sayHello("dal", 23);
-sayHello("lynn", 21);
-
-//변수명은 아무거나. 함수 정의 안에서만 존재
-function plus(firstNumber, secondNumber) {
-  console.log(firstNumber + secondNumber);
-}
-function divide(a, b) {
-  console.log(a / b);
-}
-plus(8, 60);
-divide(98, 20);
-
-const player = {
-  name: "nico",
-  sayHello: function (otherPersonName){ //함수가 데이터를 받음
-    console.log("hello " + otherPersonName + " nice to meet you!");
+//코드 숙제
+const calculator = {
+  add: function(a, b){
+    console.log(a + b);
   },
-};
+  minus: function(a, b){
+    console.log(a - b);
+  },
+  div: function(a, b){
+    console.log(a / b);
+  },
+  powerof: function(a, b){
+    console.log(a**b);
+  }
+}
 
-console.log(player.name);
-player.sayHello("lynn") //함수에 데이터를 보냄
-player.sayHello("nico")
+calculator.add(1, 2);
+calculator.minus(1, 2);
+calculator.div(1, 2);
+calculator.powerof(1, 2);
