406460f6daecc40c64cc09bfe2ac942a6e9a2371
윤지원 <133832857+rosaze@users.noreply.github.com>
2024-05-11 16:14:24 +0900

    Feat: 2.11실습 코드

--- 윤지원/강의내용 정리/5주차 강의내용정리/2.11실습.js
+++ 윤지원/강의내용 정리/5주차 강의내용정리/2.11실습.js
@@ -2,6 +2,6 @@ const age = 96;
 function calculateKrAge(ageOfForeigner) {
   return ageOfForeigner + 2;
 }
-//const krAge = calculateKrAge(age);
+const krAge2 = calculateKrAge(age);
 const krAge = calculateKrAge(96);
 console.log(krAge);
