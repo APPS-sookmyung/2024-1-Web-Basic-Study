5ace56a18babfbfc0d9e085fb60b3264df9c6a28
윤지원 <133832857+rosaze@users.noreply.github.com>
2024-05-11 16:14:46 +0900

    Feat: 2.13~2.14실습 코드

--- 윤지원/강의내용 정리/5주차 강의내용정리/2.13~2.14실습.js
+++ 윤지원/강의내용 정리/5주차 강의내용정리/2.13~2.14실습.js
@@ -1,4 +1,4 @@
-//const age = parseInt(prompt("how old are you?"));
+const age = parseInt(prompt("how old are you?"));
 console.log(typeof age); //숫자가 출력될 것이다
 const agenew = 15;
 console.log(isNaN(agenew));
