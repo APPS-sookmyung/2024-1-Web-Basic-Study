d106483caab57b5767ecad71600d881ff333609e
윤지원 <133832857+rosaze@users.noreply.github.com>
2024-05-11 16:15:54 +0900

    Feat: 3.3실습 코드

--- 윤지원/강의내용 정리/5주차 강의내용정리/3.3실습.js
+++ 윤지원/강의내용 정리/5주차 강의내용정리/3.3실습.js
@@ -1,12 +1,9 @@
 const title = document.getElementById("something");
 title.innerText = "got you!";
 console.log(title.className);
-
 const title1 = document.getElementsByTagName("h1");
 console.log(title1);
-
 const title2 = document.querySelector(".hello h1");
-
 //select all:
 const selectall = document.querySelectorAll(".hello h1");
 //returning only the first one
