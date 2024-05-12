72005666375af22e09c27ca26ee4e5a31fd83c7c
j1m1n-ha <163178666+j1m1n-ha@users.noreply.github.com>
2024-05-13 02:35:45 +0900

    Feat: #2.14 실습 코드

--- 하지민/momentum/app.js
+++ 하지민/momentum/app.js
@@ -4,4 +4,12 @@
 
 const age = parseInt(prompt("How old are you?"));
 
-console.log(age);
+console.log(isNaN(age));
+
+if (isNaN(age)) {
+  ///condition ==true
+  console.log("Please write a number");
+} else {
+  ///condition ==false
+  console.log("Thank you for writing your age.");
+}
