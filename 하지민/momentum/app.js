91cb431422d7d9316d89172088d678a96ac28772
j1m1n-ha <163178666+j1m1n-ha@users.noreply.github.com>
2024-05-13 02:29:50 +0900

    Feat: #2.13 실습 코드

--- 하지민/momentum/app.js
+++ 하지민/momentum/app.js
@@ -1,32 +1,7 @@
-// const age = 96;
-// function calculateKrAge(ageOfForeigner) {
-//   return ageOfForeigner + 2;
-// }
+// const age = prompt("How old are you?");
 
-// const krAge = calculateKrAge(age);
+// console.log(age, parseInt(age));
 
-// console.log(krAge);
+const age = parseInt(prompt("How old are you?"));
 
-const calculator = {
-  plus: function (a, b) {
-    return a + b; //return 작성하면 function 끝남
-  },
-  minus: function (a, b) {
-    console.log(a - b);
-  },
-  times: function (a, b) {
-    console.log(a * b);
-  },
-  divide: function (a, b) {
-    console.log(a / b);
-  },
-  power: function (a, b) {
-    console.log(a ** b);
-  },
-};
-
-const plusResult = calculator.plus(2, 3);
-const minusResult = calculator.minus(plusResult, 10);
-const timesResult = calculator.times(10, minusResult);
-const divideResult = calculator.divide(timesResult, plusResult);
-const powerResult = calculator.power(divideResult, minusResult);
+console.log(age);
