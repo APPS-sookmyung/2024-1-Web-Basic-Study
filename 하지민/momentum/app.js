80c8867198bc307442bb8e389256f768edfa3a95
j1m1n-ha <163178666+j1m1n-ha@users.noreply.github.com>
2024-05-13 03:27:35 +0900

    Feat: #3.2 실습 코드

--- 하지민/momentum/app.js
+++ 하지민/momentum/app.js
@@ -1,5 +1,5 @@
-const title = document.getElementById("title");
-
-title.innerText = "Got you!";
-
-console.log(title.className);
+const title = document.querySelector(".hello h1:first-child");
+// const title = document.querySelector("#hello");
+// const title = document.getElementById("hello");
+// console.log(title);
+title.innerText = "Hello";
