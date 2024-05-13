f62966fdb87fdc1e773d53f022ee1be8e3df17ce
eungrond <jeshin@sookmyung.ac.kr>
2024-05-13 15:28:44 +0900

    Feat: #2.10 실습 코드

--- 신정은/momentum/app.js
+++ 신정은/momentum/app.js
@@ -1,6 +1,30 @@
-const toBuy = ["potato", "tomato", "pizza"]
+// const player = {
+//     name: "jungeun",
+//     age: 20,
+// };
 
-console.log(toBuy);
-toBuy[2] = "water";
-console.log(toBuy);
-toBuy.push("one");
+// console.log(player);
+// player.name = "shinjungeun";
+// console.log(player);
+// player.cool = "soon";
+// console.log(player, console);
+
+//실습과제
+
+const calculator = {
+    plus: function(a,b) {
+        console.log(a+b);
+    },
+    minus: function(a,b) {
+        console.log(a-b);
+    },
+    times: function(a,b) {
+        console.log(a*b);
+    },
+    divide: function(a,b) {
+        console.log(a/b);
+    },
+    power: function(a,b) {
+        console.log(a**b);
+    },
+};
