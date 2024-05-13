08ee1d33187542c1aa6197f632b50aad11dc75c8
eungrond <jeshin@sookmyung.ac.kr>
2024-05-13 16:34:26 +0900

    Feat: #2.16 실습 코드

--- 신정은/momentum/app.js
+++ 신정은/momentum/app.js
@@ -8,8 +8,10 @@ if(isNaN(age) || age<0) {
     console.log("You are too young.");
 } else if (age>=18 && age <= 50){
     console.log("You can drink");
-} else if (age>50 && age<=88) {
+} else if (age>50 && age<=80) {
     console.log("You should exercise");
-} else if (age>88) {
+} else if (age===100) {
+    console.log("Wow you are wise");
+} else if (age>80) {
     console.log("You can do whatever you want");
 }
