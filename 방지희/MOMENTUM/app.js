6cb6cbf2c7ef234cac93390e719e8f272a09d3b4
Bangjh05 <everangel05@naver.com>
2024-05-14 00:24:47 +0900

    Feat: #2.11 실습 코드

--- 방지희/MOMENTUM/app.js
+++ 방지희/MOMENTUM/app.js
@@ -1,48 +1,35 @@
-// const player = {
-//     name: "Nico",
-//     age: 98,
-// };
-// console.log(player);
-// player.name = "nicolas";
-// console.log(player);
-// player.sexy = "soon";
-// console.log(player, console);
+const age = 96;
 
-// function plus(potato, salad) {
-//     console.log(potato + salad);
-// }
+function calculateKrAge(ageOfForeigner) {
+    ageOfForeigner + 2;
+    return "hello";
+}
+
+const krAge = calculateKrAge(age);
+console.log(krAge);
 
 
-// plus(5, 10):
-// plus(1.33453, 9898);
-// plus(9898, 1.33453);
-
-// function minusFive(potato) {
-//     console.log(potato + salad);
-// }
-
-// minusFive(5, 10, 12, 34, 4, 5, 6, 7);
 
 const calculator = {
     add: function (a, b) {
-        console.log(a + b);
+        return a + b;
     },
     min: function (a, b) {
-        console.log(a - b);
+        return a - b;
     },
     div: function (a, b) {
-        console.log(a / b);
+        return a / b;
     },
     mul: function (a, b) {
-        console.log(a * b);
+        return a * b;
     },
     powerof: function (a, b) {
-        console.log(a ** b);
-    }
+        return a ** b;
+    },
 }
 
-calculator.add(1, 2);
-calculator.min(2, 1);
-calculator.div(1, 2);
-calculator.mul(5, 3);
-calculator.powerof(2, 3);
+const addResult = calculator.add(2, 3);
+const minResult = calculator.min(addResult, 10);
+const divResult = calculator.div(10, minResult);
+const mulResult = calculator.mul(divResult, addResult);
+const powerof = calculator.powerof(divResult, minResult);
