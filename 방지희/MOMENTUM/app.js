986b99f280d167abcc5eec174645681add6434c4
Bangjh05 <everangel05@naver.com>
2024-05-13 23:58:25 +0900

    Feat: #2.10 실습 코드

--- 방지희/MOMENTUM/app.js
+++ 방지희/MOMENTUM/app.js
@@ -1,16 +1,48 @@
-// const a = 5;
-// let isNicoFat = true;
+// const player = {
+//     name: "Nico",
+//     age: 98,
+// };
+// console.log(player);
+// player.name = "nicolas";
+// console.log(player);
+// player.sexy = "soon";
+// console.log(player, console);
 
-// const a = null;
+// function plus(potato, salad) {
+//     console.log(potato + salad);
+// }
 
-// let hello;
-// console.log(hello); //umdefined
 
-// const me = "sext";
-// const days = [1, 2, false, true, null, undefined, "text", me];
+// plus(5, 10):
+// plus(1.33453, 9898);
+// plus(9898, 1.33453);
 
-// const toBuy = ["potato", "tomato", "pizza"];
+// function minusFive(potato) {
+//     console.log(potato + salad);
+// }
 
-console.log(toBuy);
-toBuy[2] = "water";
-console.log(toBuy);
+// minusFive(5, 10, 12, 34, 4, 5, 6, 7);
+
+const calculator = {
+    add: function (a, b) {
+        console.log(a + b);
+    },
+    min: function (a, b) {
+        console.log(a - b);
+    },
+    div: function (a, b) {
+        console.log(a / b);
+    },
+    mul: function (a, b) {
+        console.log(a * b);
+    },
+    powerof: function (a, b) {
+        console.log(a ** b);
+    }
+}
+
+calculator.add(1, 2);
+calculator.min(2, 1);
+calculator.div(1, 2);
+calculator.mul(5, 3);
+calculator.powerof(2, 3);
