22daae2ed89ec3a71122d546b70eb9380e1aaf57
seobin0224 <binsong2@sookmyung.ac.kr>
2024-05-14 22:46:40 +0900

    Feat: 3.0 - 3.1 실습코드

--- 윤서빈/momentum/app.js
+++ 윤서빈/momentum/app.js
@@ -1,13 +1,7 @@
-const age = parseInt(prompt("How old are you?"));
+document.title = "Hello! From JS!";
 
-if (isNaN(age) || age < 0) {
-  console.log("Please write a number");
-} else if (age < 18) {
-  console.log("You are too young.");
-} else if (age >= 18 && age <= 50) {
-  console.log("You can drink");
-} else if (age > 50 && age <= 80) {
-  console.log("You should exercise");
-} else if (age > 80) {
-  console.log("You can do whatever you want.");
-}
+const title = document.getElementById("title");
+
+title.innerText = "Got you!";
+
+console.log(title.className);
