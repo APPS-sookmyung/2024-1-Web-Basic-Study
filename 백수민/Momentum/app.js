ac46ce3d827ba5e7b7b24ce4c8424b7eee1f2b64
suminb99 <suminbaek99@gmail.com>
2024-05-14 23:05:28 +0900

    Feat: #3.2 실습 코드

--- 백수민/Momentum/app.js
+++ 백수민/Momentum/app.js
@@ -1,8 +1,15 @@
-const title = document.getElementById("title");
- 
-console.dir(title);
+const hellos = document.getElementsByClassName("hello");
 
-title.innerText = "Got you!"
+console.log(hellos);
 
-console.log(title.id);
-console.log(title.className);
+const title1 = document.getElementsByTagName("h1");
+const title2 = document.querySelector(".hello h1");
+const title3 = document.querySelectorAll(".hello h1");
+const title4 = document.querySelector(".hello h1:first-child");
+const title5 = document.querySelector("#hello"); // id: hello
+const title6 = document.getElementById("hello"); // 위 함수랑 같은 작업
+
+const title = document.querySelector(".hello h1");
+title.innerText = "hello";
+
+console.log(title);
