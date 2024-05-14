72eaf1b41c0710f094ed9c2644acc74ab49e6ee0
y-Joo492 <phyj492@naver.com>
2024-05-14 22:50:57 +0900

    Feat: #2.13 실습 코드

--- 홍연주/MOMENTUM/app.js
+++ 홍연주/MOMENTUM/app.js
@@ -1,19 +1,3 @@
-const calculator = {
-    plus: function(a, b) {
-        return a + b;
-    },
-    minus: function(a, b) {
-        return a - b;
-    },
-    times: function(a, b) {
-        return a * b;
-    },
-    divide: function(a, b) {
-        return a / b;
-    },
-    power: function(a, b) {
-        return a**b;
-    },
-};
+const age = parseInt(prompt("How ole are you?"));
 
-const plusResult = calculator.plus(2,4);
+console.log(age);
