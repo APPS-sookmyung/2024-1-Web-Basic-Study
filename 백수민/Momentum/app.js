b39d066981ecb46e86d0fdaac01f34da751bb9a6
suminb99 <suminbaek99@gmail.com>
2024-05-14 23:01:45 +0900

    Feat: #2.15 실습 코드

--- 백수민/Momentum/app.js
+++ 백수민/Momentum/app.js
@@ -1,9 +1,20 @@
 const age = parseInt(prompt("How old are you?"));
 
-console.log(isNaN(age));
+if (isNaN(age) || age < 0) {
+    console.log("Please write a real positive number.");
+} else if (age < 18) {
+    console.log("You are too young.");
+} else if (age >= 18 && age <= 50) { // both conditions need to be true to excecute the code inside
+    console.log("You can drink");
+} else if (age > 50 && age <= 80) {
+    console.log("You should exercise.");
+} else if (age === 100) {
+    console.log("wow You are wise.");
+} else if (age > 80) {
+    console.log("You can do whatever you want.")
+} 
 
-if (isNaN(age)) {
-    console.log("Please write a number");
-} else {
-    console.log("Thank you for writing your age.")
-}
+true && true === true
+false && true === false
+true && false === false
+false && false === false
