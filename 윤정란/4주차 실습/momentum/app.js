06dd267bcb83971c286799f1b6c90e158c13ea70
seobin0224 <163140905+seobin0224@users.noreply.github.com>
2024-05-14 16:57:10 +0900

    Delete 윤정란 directory

--- 윤정란/4주차 실습/momentum/app.js
+++
@@ -1,9 +0,0 @@
-const player = {
-  name: "jr",
-  sayHello: function (orderPersonName) {
-    console.log("hello " + orderPersonName + " nice to meet you");
-  },
-};
-
-player.sayHello("lynn");
-player.sayHello("nico");
