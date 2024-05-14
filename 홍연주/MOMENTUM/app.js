90bbc09b494fb4b43db7ab74d486780991e0f064
seobin0224 <163140905+seobin0224@users.noreply.github.com>
2024-05-14 16:56:07 +0900

    Delete 홍연주 directory

--- 홍연주/MOMENTUM/app.js
+++
@@ -1,9 +0,0 @@
-const player = {
-    name: "yeonju",
-    sayHello: function(otherPersonsName) {
-        console.log("hello! " + otherPersonsName + " nice to meet U!");
-    },
-};
-
-console.log(player.name);
-player.sayHello("lynn");
