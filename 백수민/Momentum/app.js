1a5dea1bafb402a7fd350476f7ef405051018724
suminb99 <suminbaek99@gmail.com>
2024-05-14 22:59:10 +0900

    Feat: #2.11 실습 코드

--- 백수민/Momentum/app.js
+++ 백수민/Momentum/app.js
@@ -1,41 +1,44 @@
-const player = {
-    name: "nico",
-    age: 98,
-};
+const age = 96;
 
-console.log(player);
-player.name = "nicolas";
-console.log(player);
-player.sexy = "soon";
-console.log(player, console);
+function calculateKrAge(ageOfForeigner) {
+    return ageOfForeigner + 2;
 
-function plus(a, b) {
-    console.log(a + b);
 }
 
-plus(5, 10);
-plus(1.33453, 9898);
-plus(9898, 1.33453);
+const krAge = calculateKrAge(age);
+
+console.log(krAge);
 
 const calculator = {
-    add: function(a, b) {
-        console.log(a + b);
+    plus: function(a, b) {
+        return a + b;
     },
 
     minus: function(a, b) {
-        console.log(a - b);
+        return a - b;
+    },
+
+    times: function(a, b) {
+        return a * b;
     },
 
     divide: function(a, b) {
-        console.log(a / b);
+        return a / b;
     }, 
 
     powerof: function(a, b) {
-        console.log(a ** b)
+        return a ** b;
     }
 }
 
-calculator.add(30, 5);
-calculator.minus(30, 5);
-calculator.divide(30, 5);
-calculator.powerof(2, 5);
+const plusResult = calculator.plus(2, 3);
+const minusResult = calculator.minus(plusResult, 10);
+const timesResult = calculator.times(10, minusResult);
+const divideResult = calculator.divide(timesResult, plusResult);
+const powerResult = calculator.powerof(divideResult, minusResult);
+
+console.log(plusResult);
+console.log(minusResult);
+console.log(timesResult);
+console.log(divideResult);
+console.log(powerResult);
