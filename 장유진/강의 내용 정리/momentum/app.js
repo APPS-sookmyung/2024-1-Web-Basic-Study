1aa2a00f4056cc8595bf73c0602045b0ff7124ff
장유진 (uzin) <139611151+janguzin@users.noreply.github.com>
2024-05-14 13:32:25 +0900

    Feat: #2.9 실습 코드

--- 장유진/강의 내용 정리/momentum/app.js
+++ 장유진/강의 내용 정리/momentum/app.js
@@ -1,20 +1,7 @@
-function plus (a,b) {
-    console.log (a + b);
-} // a,b는 이 블록 안에서만 존재
+const toBuy = ["potato", "tomato", "pizza"];
 
-function divide (first, second) {
-    console.log(first / second);
-}
-
-plus(8, 60);
-divide(98,20);
-
-const player = {
-    name: "nico",
-    sayHello: function(otherName) {
-        console.log("helo " + otherName + " nice to meet you");
-    }
-}
-
-console.log(player.name);
-player.sayHello("lynn");
+console.log(toBuy);
+toBuy[2] = "water";
+console.log(toBuy);
+toBuy.push("meat");
+console.log(toBuy);
