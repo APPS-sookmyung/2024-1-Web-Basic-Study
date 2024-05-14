60d4ee4a90c692d74be64884ffba2c1d48c73d14
Heoooooo <hayeonyu.1@gmail.com>
2024-05-14 22:09:10 +0900

    #2.13 실습 코드

--- 유하연/momentum/app.js
+++ 유하연/momentum/app.js
@@ -1,25 +1,3 @@
-const calculator = {
-    plus : function(a,b){
-        return a+b;
-    },
-    minus : function(a,b){
-        return a-b;
-    },
-    divide : function(a,b){
-        return a/b;
-    },
-    times : function(a,b){
-        return a*b;
-    },
-    power : function(a,b){
-        return a**b
-    }
+const age = parseInt(prompt("How old are you?"));
 
-};
-
-const plusResult = calculator.plus(2,3);//5
-const minusResult = calculator.minus(plusResult, 10);//-5
-const timesResult = calculator.times(10,minusResult);//-50
-const divideResult = calculator.divide(timesResult, plusResult);//-10
-const powerofResult = calculator.power(divideResult, minusResult)
-//이 코드들은 서로 의존 중이다.
+console.log(age);
