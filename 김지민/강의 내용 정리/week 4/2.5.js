989783d43dba826b97c15028e3fd8e98f548d1ce
seobin0224 <163140905+seobin0224@users.noreply.github.com>
2024-05-14 16:57:51 +0900

    Delete 김지민 directory

--- 김지민/강의 내용 정리/week 4/2.5.js
+++
@@ -1,9 +0,0 @@
-const daysOfWeek = ["mon","tue","wed","thu","fri","sat","sun"]
-
-//Get Item from Array
-console.log(daysOfWeek);
-
-//Add one more day to the Array
-daysOfWeek.push("sun")
-
-console.log(daysOfWeek);
