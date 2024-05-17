bbc9ea927e5cf0c2f7021c23557fb248d86936e7
윤지원 <133832857+rosaze@users.noreply.github.com>
2024-05-17 15:29:30 +0900

    Feat: 3.7실습 코드

---
+++ 윤지원/강의내용 정리/6주차/3.7실습.js
@@ -0,0 +1,14 @@
+const h1 = document.querySelector("div.hello:first-child h1");
+
+function handleTitleClick() {
+  h1.className = "active"; //getter이면서 setter
+}
+h1.addEventListener("click", handleTitleClick);
+function handleTitleClick() {
+  if (h1.className === "clicked") {
+    h1.className = "";
+  } else {
+    h1.className = "clicked";
+  }
+}
+h1.addEventListener("click", handleTitleClick);
