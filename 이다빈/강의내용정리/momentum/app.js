c1751493fdaa5254f8e218536ac1e812423ec208
ninum12 <ninum@sookmyung.ac.kr>
2024-05-18 08:46:33 +0900

    Feat: #3.4 실습 코드

--- 이다빈/강의내용정리/momentum/app.js
+++ 이다빈/강의내용정리/momentum/app.js
@@ -1,18 +1,20 @@
-// const title = document.querySelector(".hello:first-child h1");
-
-// console.log(title);
-
-// title.innerText = "Hello";
-
-// title.style.color = "blue" //h1 -> style -> color
-
 const title = document.querySelector("div.hello:first-child h1");
 
+//console.dir(title);
+
 function handleTitleClick() {
-  // console.log("title was clicked!");
   title.style.color = "blue"
 }
 
+function handleMouseEnter(){
+  // console.log("mouse is here!");
+  title.innerText = "Mouse is here!"
+}
+
+function handleMouseLeave(){
+  title.innerText = "Mouse is gone!"
+}
+
 title.addEventListener("click", handleTitleClick);
-//두번째 인수로 함수를 전달: "click"할 경우에 함수가 실행되게 함.
-//JavaScript에 함수이름을 넘겨 JavaScript가 대신 실행버튼을 누르게 함.
+title.addEventListener("mouseenter", handleMouseEnter);
+title.addEventListener("mouseleave", handleMouseLeave);
--- 이다빈/강의내용정리/week6.md
+++ 이다빈/강의내용정리/week6.md
@@ -1,4 +1,11 @@
 # 3.3
 .id:first-child 하위tag이름
 event를 listen 하는 법
-addEventListener()
+HTMLelement.addEventListener("event", function);
+
+# 3.4
+HTMLElement 찾기
+->MDN(Mozilla Developer Network)검색
+->Web APIs 찾기
+or
+console.dir()
