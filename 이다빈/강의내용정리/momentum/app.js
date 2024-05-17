e93ea235b1feab90967a534e0761c06b0597f20e
ninum12 <ninum@sookmyung.ac.kr>
2024-05-18 08:36:42 +0900

    Feat: #3.3 실습 코드

--- 이다빈/강의내용정리/momentum/app.js
+++ 이다빈/강의내용정리/momentum/app.js
@@ -1,12 +1,18 @@
-// const title = document.getElementsByTagName("h1"); //배열로 가져옴
-const title = document.querySelector(".hello h1");
-const titles = document.querySelectorAll(".hello h1");
+// const title = document.querySelector(".hello:first-child h1");
 
-// const title = document.querySelector("#hello");
-// const title = document.getElementById("hello");
-// 두 개가 같음
+// console.log(title);
 
-title.innerText = "Hello";
+// title.innerText = "Hello";
 
-console.log(title);
-console.log(titles);
+// title.style.color = "blue" //h1 -> style -> color
+
+const title = document.querySelector("div.hello:first-child h1");
+
+function handleTitleClick() {
+  // console.log("title was clicked!");
+  title.style.color = "blue"
+}
+
+title.addEventListener("click", handleTitleClick);
+//두번째 인수로 함수를 전달: "click"할 경우에 함수가 실행되게 함.
+//JavaScript에 함수이름을 넘겨 JavaScript가 대신 실행버튼을 누르게 함.
---
+++ 이다빈/강의내용정리/week6.md
@@ -0,0 +1,4 @@
+# 3.3
+.id:first-child 하위tag이름
+event를 listen 하는 법
+addEventListener()
