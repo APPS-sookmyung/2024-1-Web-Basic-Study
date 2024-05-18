69379069c9d639c850435ccccb102c193b92a183
ninum12 <ninum@sookmyung.ac.kr>
2024-05-18 10:04:28 +0900

    Feat: #3.6 실습 코드

--- 이다빈/강의내용정리/momentum/app.js
+++ 이다빈/강의내용정리/momentum/app.js
@@ -1,39 +1,24 @@
 const h1 = document.querySelector("div.hello:first-child h1");
 
 function handleTitleClick() {
-  h1.style.color = "blue"
-}
+  // console.log(h1.style.color);
+  // h1.style.color = "blue";
+  // console.log(h1.style.color);
 
-function handleMouseEnter(){
-  h1.innerText = "Mouse is here!"
-}
+  // if(h1.style.color === "blue"){
+  //   h1.style.color = "tomato"
+  // } else {
+  //   h1.style.color = "blue"
+  // }
 
-function handleMouseLeave(){
-  h1.innerText = "Mouse is gone!"
+  const currentColor = h1.style.color;
+  let newColor;
+  if(currentColor === "blue"){
+    newColor = "tomato"
+  } else {
+    newColor = "blue"
+  }
+  h1.style.color = newColor;
 }
-
-function handleWindowResize(){
-  document.body.style.backgroundColor = "tomato";
-}
-
-function handleWindowCopy(){
-  alert("copier!");
-}
-
-function handleWindowOffline(){
-  alert("SOS no WIFI");
-}
-
-function handleWindowOnline(){
-  alert("ALL GOOOD");
-}
-
 
 h1.addEventListener("click", handleTitleClick);
-h1.addEventListener("mouseenter", handleMouseEnter);
-h1.addEventListener("mouseleave", handleMouseLeave);
-
-window.addEventListener("resize", handleWindowResize);
-window.addEventListener("copy", handleWindowCopy);
-window.addEventListener("offline", handleWindowOffline);
-window.addEventListener("online", handleWindowOnline);
--- 이다빈/강의내용정리/week6.md
+++ 이다빈/강의내용정리/week6.md
@@ -7,5 +7,17 @@ HTMLelement.addEventListener("event", function);
 HTMLElement 찾기
 ->MDN(Mozilla Developer Network)검색
 ->Web APIs 찾기
+(https://developer.mozilla.org/en-US/docs/Web/API/Window)
 or
 console.dir()
+
+# 3.5
+더 많은 event
+title.addEventListenter("click", handleTitleClick)
+title.onclick = handleTitleClick
+동일하나
+addEventListenter을 이용하면 removeEventListener을 통해 event listener을 제거할 수 있음
+event를 listen해서 이벤트를 처리한다
+
+# 3.6
+조건문을 활용해 클릭시 현재 색에 따라 색 변경하기
