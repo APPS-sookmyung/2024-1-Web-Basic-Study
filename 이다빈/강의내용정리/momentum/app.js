69c147004905475b4db14a66c2ee00783f98b9a1
ninum12 <ninum@sookmyung.ac.kr>
2024-05-18 10:22:54 +0900

    Feat: #3.7 실습 코드

--- 이다빈/강의내용정리/momentum/app.js
+++ 이다빈/강의내용정리/momentum/app.js
@@ -1,24 +1,13 @@
 const h1 = document.querySelector("div.hello:first-child h1");
 
 function handleTitleClick() {
-  // console.log(h1.style.color);
-  // h1.style.color = "blue";
-  // console.log(h1.style.color);
-
-  // if(h1.style.color === "blue"){
-  //   h1.style.color = "tomato"
-  // } else {
-  //   h1.style.color = "blue"
-  // }
-
-  const currentColor = h1.style.color;
-  let newColor;
-  if(currentColor === "blue"){
-    newColor = "tomato"
+  // h1.className = "active";
+  const clickedClass = "clicked"
+  if (h1.className === clickedClass){
+    h1.className = "";
   } else {
-    newColor = "blue"
+    h1.className = clickedClass;
   }
-  h1.style.color = newColor;
 }
 
 h1.addEventListener("click", handleTitleClick);
--- 이다빈/강의내용정리/week6.md
+++ 이다빈/강의내용정리/week6.md
@@ -21,3 +21,12 @@ event를 listen해서 이벤트를 처리한다
 
 # 3.6
 조건문을 활용해 클릭시 현재 색에 따라 색 변경하기
+
+# 3.7
+className은 getter이면서 setter
+class를 얻어올 수도 있고 변경할 수도 있음
+css에서 생성한 class를 이용해 색 변경하기
+transition을 추가해 변화 속도 설정하기
+개발자가 설정한 raw string(raw value)을 직접 사용하면 오타등의 오류 발생
+->변수에 저장
+모든 class name을 변경하지 않게 해야함.
