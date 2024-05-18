ce25555ad01967274f8b86c00c1f661d31ddb096
ninum12 <ninum@sookmyung.ac.kr>
2024-05-18 20:22:56 +0900

    Feat: #4.1 실습 코드

--- 이다빈/강의내용정리/momentum/app.js
+++ 이다빈/강의내용정리/momentum/app.js
@@ -1,14 +1,15 @@
-// const loginForm = document.getElementById("login-form");
-// const loginInput = loginForm.querySelector("input");
-// const loginButton = loginForm.querySelector("button");
-
 const loginInput = document.querySelector("#login-form input");
 const loginButton = document.querySelector("#login-form button");
 
 
 function handleLoginBtnClick() {
-  console.dir(loginInput.value);
-  // console.log("click!!!!")
+  const username = loginInput.value;
+  // if (username === "") {
+  //   alert("Please write your name");
+  // } else if(username.length > 15) {
+  //     alert("Your name is too long.")
+  // }
+  console.log(username);
 }
 
 loginButton.addEventListener("click", handleLoginBtnClick);
