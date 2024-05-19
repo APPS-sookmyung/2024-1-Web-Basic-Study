134f3a9d31a1908d3788e38445436a90a4a135a6
Bangjh05 <everangel05@naver.com>
2024-05-19 22:32:30 +0900

    Feat: #4.6 실습 코드

--- 방지희/MOMENTUM/app.js
+++ 방지희/MOMENTUM/app.js
@@ -2,16 +2,28 @@ const loginInput = document.querySelector("#loin-form");
 const loginButton = document.querySelector("#login-form input");
 const greeting = document.querySelector("#greeting");
 const HIDDEN_CLASSNAME = "hidden";
-
-const link = document.querySelector("a");
+const USERNAME_KEY = "username";
 
 function onLoginSubmit(event) {
     event.preventDefault();
     loginForm.classList.add(HIDDEN_CLASSNAME);
     const username = loginInput.value;
-    localStorage.setItem("username", username);
+    localStorage.setItem(USERNAME_KEY, username);
+    paintGreetings(username);
+}
+
+function paintGreetings(username) {
     greeting.innerText = `Hello ${username}`;
     greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
-loginForm.addEventListener("submit", onLoginSubmit);
+const savedUername = localStorage.getItem(USERNAME_KEY);
+
+if(savedUername === null) {
+    //show the form
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener("submit", onLoginSubmit);
+} else {
+    //show the greetings
+    paintGreetings(savedUername);
+}
