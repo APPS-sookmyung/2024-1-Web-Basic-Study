3f424c37f7104cac7c19df22f815f972a4bc6537
장유진 (uzin) <139611151+janguzin@users.noreply.github.com>
2024-05-20 17:22:40 +0900

    Feat: #3.8 실습 코드

--- 장유진/강의 내용 정리/momentum/app.js
+++ 장유진/강의 내용 정리/momentum/app.js
@@ -1,11 +1,13 @@
 const h1 = document.querySelector("div.hello:first-child h1");
 
 function handleTitleClick() {
-    if (h1.className === "active") {
-        h1. className = "";
+    const clickedClass = "clicked";
+    if (h1.classList.contains(clickedClass)) {
+        h1.classList.remove(clickedClass);
     } else {
-        h1.className = "active";
+        h1.classList.add(clickedClass);
     }
+    h1.classList.toggle("clicked");
 }
 
 h1.addEventListener("click", handleTitleClick);
