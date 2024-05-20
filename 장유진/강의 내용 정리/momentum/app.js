d982b1b94dedf95d69add0b0343ae9dee7a64924
장유진 (uzin) <139611151+janguzin@users.noreply.github.com>
2024-05-20 17:05:31 +0900

    Feat: #3.5 실습 코드

--- 장유진/강의 내용 정리/momentum/app.js
+++ 장유진/강의 내용 정리/momentum/app.js
@@ -12,6 +12,33 @@ function handleMouseLeave() {
     title.innerText = "Mouse is gone!";
 }
 
+function handleWindowResize() {
+    document.body.style.backgroundColor = "tomato";
+}
+
 title.addEventListener("click", handleTitleClick);
 title.addEventListener("mouseenter", handleMouseEnter);
 title.addEventListener("mouseleave", handleMouseLeave);
+
+
+
+function handleWindowCopy() {
+    alert("copier!");
+}
+
+function handleWindowOffline() {
+    alert("SOS no WIFI");
+}
+
+function handleWindowOnline() {
+    alert("ALL GOOD!");
+}
+
+h1.addEventListener("click", handleTitleClick);
+h1.addEventListener("mouseenter", handleMouseEnter);
+h1.addEventListener("mouseleave", handleMouseLeave);
+
+window.addEventListener("resize", handleMouseLeave);
+window.addEventListener("copy", handleWindowCopy);
+window.addEventListener("resize", handleWindowOffline);
+window.addEventListener("online", handleWindowOnline);
