69074347fc8e19b28f0df88bed50103129b5b3a7
장유진 (uzin) <139611151+janguzin@users.noreply.github.com>
2024-05-20 17:57:30 +0900

    Feat: #4.6 실습 코드

--- 장유진/강의 내용 정리/momentum/app.js
+++ 장유진/강의 내용 정리/momentum/app.js
@@ -1,18 +1,27 @@
 const loginInput = document.querySelector("#login-form");
 const loginButton = document.querySelector("#login-form input");
 
-const link = document.querySelector("a");
+const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
 
 function onLoginSubmit(event) {
     event.preventDefault();
     loninForm.classList.add(HIDDEN_CLASSNAME);
     const username = loginInput.value;
-    "Hello" + username
-    'Hello ${username}'
-    greeting.innerText = 'Hello ${username}';
+    localStorage.setItem(USERNAME_KEY, username);
+    paintGreetings(username);
+}
+
+function paintGreetings(username) {
+    greeting.innerText = 'Hello ${usernaem}';
     greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
+const savedUsername = localStorage.getItem(USERNAME_KEY);
 
-loginForm.addEventListener("submit", onLoginSubmit);
-link.addEventListener("click", handleLinkClick);
+if (savedUsername === null) {
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener("submit", onLoginSubmit);
+} else {
+    paintGreetings(savedUsername);
+}
