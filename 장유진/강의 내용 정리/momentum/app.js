e2942f693f2497b109b62437e162579a667c3bd9
장유진 (uzin) <139611151+janguzin@users.noreply.github.com>
2024-05-20 17:13:49 +0900

    Feat: #3.6 실습 코드

--- 장유진/강의 내용 정리/momentum/app.js
+++ 장유진/강의 내용 정리/momentum/app.js
@@ -1,44 +1,13 @@
-const title = document.querySelector("div.hello:first-child h1");
+const h1 = document.querySelector("div.hello:first-child h1");
 
 function handleTitleClick() {
-    title.style.color = "blue";
+    const currentColor = h1.computedStyleMap.color;
+    let newColor;
+    if (currentColor = "blue") {
+        newColor = "tomato";
+    } else {
+        newColor = "blue";
+    }
+    h1.computedStyleMap.color = newColor;
+
 }
-
-function handleMouseEnter() {
-    title.innerText = "Mouse is here!";
-}
-
-function handleMouseLeave() {
-    title.innerText = "Mouse is gone!";
-}
-
-function handleWindowResize() {
-    document.body.style.backgroundColor = "tomato";
-}
-
-title.addEventListener("click", handleTitleClick);
-title.addEventListener("mouseenter", handleMouseEnter);
-title.addEventListener("mouseleave", handleMouseLeave);
-
-
-
-function handleWindowCopy() {
-    alert("copier!");
-}
-
-function handleWindowOffline() {
-    alert("SOS no WIFI");
-}
-
-function handleWindowOnline() {
-    alert("ALL GOOD!");
-}
-
-h1.addEventListener("click", handleTitleClick);
-h1.addEventListener("mouseenter", handleMouseEnter);
-h1.addEventListener("mouseleave", handleMouseLeave);
-
-window.addEventListener("resize", handleMouseLeave);
-window.addEventListener("copy", handleWindowCopy);
-window.addEventListener("resize", handleWindowOffline);
-window.addEventListener("online", handleWindowOnline);
