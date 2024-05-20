445676ab03f2b01b4f9ade7ea7867a082d7bb2f2
suminb99 <suminbaek99@gmail.com>
2024-05-21 02:24:42 +0900

    Feat: #4.6 실습 코드

--- 백수민/Momentum/app.js
+++ 백수민/Momentum/app.js
@@ -3,14 +3,28 @@ const loginInput = document.querySelector("#login-form input");
 const greeting = document.querySelector("#greeting");
 
 const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
 
 function onLoginSubmit(event) {
     event.preventDefault(); // 새로고침 막아줌
     loginForm.classList.add(HIDDEN_CLASSNAME)
     const username = loginInput.value;
-    localStorage.setItem("username", username);
+    localStorage.setItem(USERNAME_KEY, username);
+    paintGreetings(username);
+}
+
+loginForm.addEventListener("submit", onLoginSubmit);
+
+function paintGreetings(username) {
     greeting.innerText = `Hello ${username}`;
     greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
-loginForm.addEventListener("submit", onLoginSubmit);
+const savedusername = localStorage.getItem(USERNAME_KEY);
+
+if (savedusername === null) {
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener("submit", onLoginSubmit);
+} else {
+    paintGreetings(savedusername);
+}
