1f3c78d39a13509c896c527597137384fc992b69
suminb99 <suminbaek99@gmail.com>
2024-05-21 00:57:31 +0900

    Feat: #4.3 실습 코드

--- 백수민/Momentum/app.js
+++ 백수민/Momentum/app.js
@@ -1,9 +1,16 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
 
+const link = document.querySelector("a");
+
 function onLoginSubmit(event) {
     event.preventDefault(); // 새로고침 막아줌
     console.log(loginInput.value);
 }
 
+function handleLinkClick(event) {
+    event.preventDefault();
+}
+
 loginForm.addEventListener("submit", onLoginSubmit);
+link.addEventListener("submit", handleLinkClick);
