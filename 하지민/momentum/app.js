692bf70ef9d82bb0ee182054bafc9a2d6d7d37a7
j1m1n-ha <163178666+j1m1n-ha@users.noreply.github.com>
2024-05-20 09:15:32 +0900

    Feat: #4.0 실습 코드

--- 하지민/momentum/app.js
+++ 하지민/momentum/app.js
@@ -1,13 +1,9 @@
-const h1 = document.querySelector("div.hello:first-child h1");
+// const loginForm = document.getElementById("login-form");
+const loginInput = document.querySelector("#login-form input");
+const loginButton = document.querySelector("#login-form button");
 
-function handleTitleClick() {
-  const clickedClass = "clicked";
-  //   if (h1.classList.contains(clickedClass)) {
-  //     h1.classList.remove(clickedClass);
-  //   } else {
-  //     h1.classList.add(clickedClass);
-  //   }
-  h1.classList.toggle("clicked");
+function onLoginBtnClick() {
+  console.log("hello", loginInput.value);
 }
 
-h1.addEventListener("click", handleTitleClick);
+loginButton.addEventListener("click", onLoginBtnClick);
