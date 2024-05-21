a69e07e55f73e966353d4c6b6069666b98499918
seobin0224 <binsong2@sookmyung.ac.kr>
2024-05-21 12:40:55 +0900

    Feat: 3.7 실습 코드

--- 윤서빈/momentum/app.js
+++ 윤서빈/momentum/app.js
@@ -1,14 +1,9 @@
 const h1 = document.querySelector("div.hello:first-child h1");
-
 function handleTitleClick() {
-  const currentColor = h1.style.color;
-  let newColor;
-  if (currentColor === "blue") {
-    newColor = "tomato";
+  if (h1.className === "clicked") {
+    h1.className = "";
   } else {
-    newColor = "blue";
+    h1.className = "clicked";
   }
-  h1.style.color = newColor;
 }
-
 h1.addEventListener("click", handleTitleClick);
