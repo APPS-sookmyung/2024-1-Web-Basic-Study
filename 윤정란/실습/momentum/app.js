5fb8982b0e737c1fb4ef4ed54149b29f5dc83d2c
yoon-jr <yojr3001@sookmyung.ac.kr>
2024-05-21 21:15:28 +0900

    Feat: 4.4 실습

--- 윤정란/실습/momentum/app.js
+++ 윤정란/실습/momentum/app.js
@@ -1,9 +1,13 @@
 const loginForm = document.querySelector("#login");
 const loginInput = loginForm.querySelector("input");
+const greeting = document.querySelector("#greeting");
 
-function login(tomato) {
-  tomato.prerventDefault();
-  console.log(tomato);
+function login(event) {
+  event.preventDefault();
+  const username = loginInput.value;
+  loginForm.classList.add("hidden");
+  greeting.innerHTML = "hello!";
+  greeting.classList.remove("hidden");
 }
 
 loginForm.addEventListener("submit", login);
