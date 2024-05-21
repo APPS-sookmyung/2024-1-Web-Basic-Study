dfeb6279731608b5339cbcbceba1a0714f6e3ca9
yoon-jr <yojr3001@sookmyung.ac.kr>
2024-05-21 19:51:44 +0900

    Feat: 3.3 실습

--- 윤정란/실습/momentum/app.js
+++ 윤정란/실습/momentum/app.js
@@ -1,9 +1,5 @@
-const player = {
-  name: "jr",
-  sayHello: function (orderPersonName) {
-    console.log("hello " + orderPersonName + " nice to meet you");
-  },
-};
-
-player.sayHello("lynn");
-player.sayHello("nico");
+const title = document.querySelector("div.hello:first-child h1");
+function click() {
+  console.log("title was clicked!");
+}
+title.addEventListener("click", click);
