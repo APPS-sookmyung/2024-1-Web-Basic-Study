eca6bb75a3ac0bd6f0e53870332ac07e8e2b0ba0
seobin0224 <binsong2@sookmyung.ac.kr>
2024-05-21 12:45:04 +0900

    Feat: 3.8 실습코드

--- 윤서빈/momentum/app.js
+++ 윤서빈/momentum/app.js
@@ -1,9 +1,9 @@
 const h1 = document.querySelector("div.hello:first-child h1");
 function handleTitleClick() {
-  if (h1.className === "clicked") {
-    h1.className = "";
+  const clickedClass = "clicked";
+  if (h1.classList.contains(clickedClass)) {
+    h1.classList.remove(clickedClass);
   } else {
-    h1.className = "clicked";
+    h1.classList.add(clickedClass);
   }
 }
-h1.addEventListener("click", handleTitleClick);
