90c6406ac89b382ed3a490d73705091d7257eb0d
yoon-jr <yojr3001@sookmyung.ac.kr>
2024-05-21 20:30:27 +0900

    Feat: 3.8 실습

--- 윤정란/실습/momentum/app.js
+++ 윤정란/실습/momentum/app.js
@@ -1,10 +1,13 @@
 const title = document.querySelector("div.hello:first-child h1");
 function click() {
-  if (title.className === "active") {
-    title.className = "";
-  } else {
-    title.className = "active";
-  }
+  title.classList.toggle("active");
+
+  // const activeClass = "active";
+  // if (title.classList.contains(activeClass)) {
+  // title.classList.remove(activeClass);
+  // } else {
+  // title.classList.add(activeClass);
+  // }
 }
 
 title.onclick = click;
