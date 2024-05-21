8b49ee162f2f01b9e4908da91e7e0a1b05823589
seobin0224 <binsong2@sookmyung.ac.kr>
2024-05-21 13:26:27 +0900

    Feat: 4.4 실습코드

--- 윤서빈/momentum/app.js
+++ 윤서빈/momentum/app.js
@@ -1,20 +1,16 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
+const greeting = document.querySelector("#greeting");
 
-const link = document.querySelector("a");
+const HIDDEN_CLASSNAME = "hidden";
 
 function onLoginSubmit(event) {
   event.preventDefault();
-  // 브라우저가 기본 동작을 실행하지 못하게 막기
-  // event object는 preventDefault함수를 기본적으로 갖고 있음
-  console.log(loginInput.value);
-}
-
-function handleLinkClick() {
-  alert("click");
+  loginForm.classList.add(HIDDEN_CLASSNAME);
+  const username = loginInput.value;
+  // greeting.innerText = "Hello " + username;
+  greeting.innerText = `Hello ${username}`;
+  greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
 loginForm.addEventListener("submit", onLoginSubmit);
-// submit 이벤트가 발생한다면, onLoginSubmit함수를 실행시킨다는 의미
-// JS는 onLoginSubmit함수 호출시 인자를 담아서 호출함. 해당 인자는 event object를 담은 정보들
-link.addEventListener("click", handleLinkClick);
