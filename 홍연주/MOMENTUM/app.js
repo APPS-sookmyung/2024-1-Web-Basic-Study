cc39291cfff1e225947528a8d9ea1e09b1d1b261
y-Joo492 <phyj492@naver.com>
2024-05-21 22:45:34 +0900

    Feat: #4.4 실습 코드

--- 홍연주/MOMENTUM/app.js
+++ 홍연주/MOMENTUM/app.js
@@ -1,18 +1,17 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
-const loginButton = document.querySelector("#login-form button");
+const greeting = document.querySelector("#greeting");
 
-const link = document.querySelector("a");
+const HIDDEN_CLASSNAME = "hidden";
+
 
 function onLoginSubmit(event) {
     event.preventDefault();
-    console.log(loginInput.value);
-}
-
-function handleLinkClick(event) {
-    event.preventDefault();
-    console.log(event);
-    alert("click!");
+    loginForm.classList.add(HIDDEN_CLASSNAME);
+    const username = loginInput.value;
+    greeting.innerHTML = "Hello " + username;
+    greeting.innerHTML = `Hello ${username}`;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
 loginForm.addEventListener("submit", onLoginSubmit);
