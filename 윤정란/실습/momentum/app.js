a6dff7c37a92cc83ccb27ce471f78cbc79d8e09f
yoon-jr <yojr3001@sookmyung.ac.kr>
2024-05-21 20:49:57 +0900

    Feat: 4.1 실습

--- 윤정란/실습/momentum/app.js
+++ 윤정란/실습/momentum/app.js
@@ -3,6 +3,11 @@ const loginInput = loginForm.querySelector("input");
 const loginButton = loginForm.querySelector("button");
 
 function click() {
-  console.log(loginInput.value);
+  const username = loginInput.value;
+  if (username === "") {
+    alert("이름을 적어 주세요");
+  } else if (username.length > 15) {
+    alert("이름이 너무 깁니다");
+  }
 }
 loginButton.addEventListener("click", click);
