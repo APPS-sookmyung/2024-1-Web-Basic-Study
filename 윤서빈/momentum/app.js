e335b15971142016a7f570ed7c230d6ce266e160
seobin0224 <binsong2@sookmyung.ac.kr>
2024-05-21 13:00:01 +0900

    Feat: 4.1 실습코드

--- 윤서빈/momentum/app.js
+++ 윤서빈/momentum/app.js
@@ -1,8 +1,8 @@
 const loginInput = document.querySelector(".form input");
 const loginButton = document.querySelector(".form button");
 
-function ClickButton() {
-  console.log(loginInput.value); // 버튼을 눌렀을때 input(텍스트박스) 통해서 입력한것들이 출력된다
-  console.log("click");
+function onLoginBTnClick() {
+  const username = loginInput.value;
+  console.log(username);
 }
-loginButton.addEventListener("click", ClickButton);
+loginButton.addEventListener("click", onLoginBTnClick);
