fbbc1ff67d6354479565323f9ef60780f3e70680
Heoooooo <hayeonyu.1@gmail.com>
2024-05-21 20:20:17 +0900

    Feat: #4.4 실습 코드

--- 유하연/momentum/app.js
+++ 유하연/momentum/app.js
@@ -1,9 +1,17 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form Input");
+const greeting = document.querySelector("#greeting");
+
+const HIDDEN_CLASSNAME = "hidden";
 
 function onLoginSubmit(event){
     event.preventDefault();
-    console.log(loginInput.value);
-}
+    loginForm.classList.add("hidden");
+    const username = loginInput.value;
+    greeting.innerText = "Hello"+username;
+    
+    greeting.classList.remove(HIDDEN_CLASSNAME);
+} 
+
 
 loginForm.addEventListener("submit", onLoginSubmit);
