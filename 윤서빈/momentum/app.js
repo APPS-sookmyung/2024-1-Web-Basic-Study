18f140e4bd426e33d00f71eea05bc28cb872369f
seobin0224 <binsong2@sookmyung.ac.kr>
2024-05-21 12:51:30 +0900

    Feat: 4.0 실습코드

--- 윤서빈/momentum/app.js
+++ 윤서빈/momentum/app.js
@@ -1,9 +1,8 @@
-const h1 = document.querySelector("div.hello:first-child h1");
-function handleTitleClick() {
-  const clickedClass = "clicked";
-  if (h1.classList.contains(clickedClass)) {
-    h1.classList.remove(clickedClass);
-  } else {
-    h1.classList.add(clickedClass);
-  }
+const loginInput = document.querySelector(".form input");
+const loginButton = document.querySelector(".form button");
+
+function ClickButton() {
+  console.log(loginInput.value); // 버튼을 눌렀을때 input(텍스트박스) 통해서 입력한것들이 출력된다
+  console.log("click");
 }
+loginButton.addEventListener("click", ClickButton);
