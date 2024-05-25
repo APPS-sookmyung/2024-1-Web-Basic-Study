60831189d1eb284ab2549b40a813d2e1c39b7af9
seobin0224 <binsong2@sookmyung.ac.kr>
2024-05-25 16:04:25 +0900

    Feat: 5.2 실습코드

--- 윤서빈/momentum/js/clock.js
+++ 윤서빈/momentum/js/clock.js
@@ -2,9 +2,10 @@ const clock = document.querySelector("h2#clock");
 
 function getClock() {
   const date = new Date();
-  clock.innerText = `${date.getHours()}:${date.getMinutes()}:${
-    date.getSeconds
-  }}`;
+  const hours = string(date.getHours()).padstart(2, "0");
+  const minutes = string(date.getMinutes()).padstart(2, "0");
+  const seconds = string(date.getSeconds()).padstart(2, "0");
+  clock.innerText = `${hours}:${minutes}:${seconds}`;
 }
 
 getClock();
