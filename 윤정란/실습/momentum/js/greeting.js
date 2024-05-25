31bdf6b035d7bae9313e9e774261791a6096d8a1
yoon-jr <yojr3001@sookmyung.ac.kr>
2024-05-25 17:36:48 +0900

    7.0 실습

--- 윤정란/실습/momentum/js/greeting.js
+++ 윤정란/실습/momentum/js/greeting.js
@@ -1,1 +1,27 @@
+const loginForm = document.querySelector("#login");
+const loginInput = loginForm.querySelector("input");
+const greeting = document.querySelector("#greeting");
 
+function login(event) {
+  event.preventDefault();
+  const username = loginInput.value;
+  loginForm.classList.add("hidden");
+  localStorage.setItem("username", username);
+  showGreeting(username);
+}
+
+function showGreeting(username) {
+  greeting.classList.remove("hidden");
+  greeting.innerHTML = `hello ${username}`;
+}
+
+loginForm.addEventListener("submit", login);
+
+const savedUsername = localStorage.getItem("username");
+
+if (savedUsername === null) {
+  loginForm.classList.remove("hidden");
+} else {
+  loginForm.classList.add("hidden");
+  showGreeting(savedUsername);
+}
