5dbaf64818e3f5ca496a82497b0b6262c521f108
eungrond <jeshin@sookmyung.ac.kr>
2024-05-25 16:59:31 +0900

    Feat: #7.3 실습 코드

--- 신정은/7주차 과제/momentum/js/todo.js
+++ 신정은/7주차 과제/momentum/js/todo.js
@@ -2,8 +2,14 @@ const toDoForm = document.getElementById("todo-form");
 const toDoInput = document.querySelector("#todo-form input");
 const toDoList = document.getElementById("todo-list");
 
+const toDos = [];
+
+function saveToDos() {
+    localStorage.setItem("todos", JSON.stringify(todos));
+}
+
 function deleteToDo(event) {
-    console.log(event.target.parentElement);
+    const li = event.target.parentElement;
     li.remove();
 }
 
@@ -23,6 +29,7 @@ function handleToDoSubmit(event) {
     event.preventDefault();
     const newTodo = toDoInput.value;
     toDoInput.value = "";
+    toDos.push(newTodo);
     paintToDo(newTodo);
 }
 
