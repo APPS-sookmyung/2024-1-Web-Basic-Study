7ad1164d9dc029c956a9bac8cc92191b45606ca0
suminb99 <suminbaek99@gmail.com>
2024-05-26 16:23:23 +0900

    Feat: #5.1 실습 코드

--- 백수민/Momentum/clock.js
+++ 백수민/Momentum/clock.js
@@ -1,7 +1,9 @@
-const clock = document.querySelector("h2#clock")
+const clock = document.querySelector("h2#clock");
 
-function sayHello() {
-    console.log("hello")
+function getClock() {
+    const date = new Date();
+    clock.innerText = (`${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`);
 }
 
-setInterval(sayHello, 5000);
+getClock(); // 바로 실행시키기
+setInterval(getClock, 1000);
