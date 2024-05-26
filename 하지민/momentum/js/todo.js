89d56f221d4b9077113200e58540644ba0c07a92
j1m1n-ha <163178666+j1m1n-ha@users.noreply.github.com>
2024-05-26 16:50:41 +0900

    Feat: #7.3 실습 코드

--- 하지민/momentum/js/todo.js
+++ 하지민/momentum/js/todo.js
@@ -2,15 +2,19 @@ const toDoForm = document.getElementById("todo-form");
 const toDoInput = document.querySelector("#todo-form input");
 const toDoList = document.getElementById("todo-list");
 
+const toDos = [];
+
+function saveToDos() {
+  localStorage.setItem("todos", JSON.stringify(toDos));
+}
+
 function deleteToDo(event) {
   const li = event.target.parentElement;
   li.remove();
 }
-
 function paintToDo(newTodo) {
   const li = document.createElement("li");
   const span = document.createElement("span");
-  li.appendChild(span);
   span.innerText = newTodo;
   const button = document.createElement("button");
   button.innerText = "❌";
@@ -19,3 +23,13 @@ function paintToDo(newTodo) {
   li.appendChild(button);
   toDoList.appendChild(li);
 }
+function handleToDoSubmit(event) {
+  event.preventDefault();
+  const newTodo = toDoInput.value;
+  toDoInput.value = "";
+  toDos.push(newTodo);
+  paintToDo(newTodo);
+  saveToDos();
+}
+
+toDoForm.addEventListener("submit", handleToDoSubmit);
