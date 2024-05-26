621c7329cc41c4d7fd45b5616a6055e766f7dc09
suminb99 <suminbaek99@gmail.com>
2024-05-26 17:04:10 +0900

    Feat: #6.0 실습 코드

--- 백수민/Momentum/js/clock.js
+++ 백수민/Momentum/js/clock.js
@@ -1,7 +1,7 @@
 const clock = document.querySelector("h2#clock");
 
 function getClock() {
-    const date = new Date();
+    const date = new Date(); // date 객체 생성
     const hours = String(date.getHours()).padStart(2, "0")
     const minutes = String(date.getMinutes()).padStart(2, "0")
     const seconds = String(date.getSeconds()).padStart(2, "0")
