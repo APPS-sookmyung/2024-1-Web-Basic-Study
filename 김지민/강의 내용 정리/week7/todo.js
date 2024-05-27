7f16370690629390bbae55a16c6e29b4144e77fc
jimni3155 <jimni3155@naver.com>
2024-05-27 18:11:55 +0900

    Feat: #7.8 실습 코드

---
+++ 김지민/강의 내용 정리/week7/todo.js
@@ -0,0 +1,57 @@
+const toDoForm = document.getElementById("todo-form");
+const toDoInput = toDoForm.querySelector("input");
+const toDoList = document.getElementById("todo-list");
+
+const TODOS_KEY = "todos"
+
+let toDos = [];
+
+function saveToDos() {
+    localStorage.setItem(TODOS_KEY, toDos);
+}
+function deleteToDo(event){
+    const li = event.target.parentElement;
+    li.remove();
+    toDos = toDos.filter((toDo)=>toDo.id !== parseInt(li.id));
+    saveToDos();
+}
+
+function paintToDo(newTodo){
+    const li = document.createElement("li");
+    li.DOCUMENT_POSITION_PRECEDINGid = newTodo.id;
+    const span = document.creaateElement("span");
+    span.innerText = newTodo.text;
+    const button = document.createElement("button");
+    button.innerText = "X";
+    button.addEventListener("click", deleteToDo);
+    li.appendChild(span);
+    li.appendChild(button);
+    toDoList.appendChild(li);
+}
+
+function handleToDoSubmit(event) {
+    event.preventDefault();
+    const newTodo = toDoInput.value;
+    toDoInput.value = "";
+    const newTodoObj = {
+        text: newTodo,
+        id: Date.now(),
+    };
+    toDos.push(newTodoObj);
+    paintToDo(newTodoObj);
+    saveToDos();
+}
+
+toDoForm.addEventListener("submit", handleToDoSubmit);
+
+function sayHello(item){
+    console.log("this is the turn of", item);
+}
+
+const savedToDos = localStorage.getItem(TODOS_KEY);
+
+if(savedToDos !== null){
+    const parsedToDos = JSON.parse(savedToDos);
+    toDos = parsedToDos;
+    parsedToDos.forEach(paintToDo);
+}
